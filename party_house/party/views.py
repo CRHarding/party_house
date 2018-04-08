21071d523b52dec5a501ac0844adebe5d1ceda9d
CRHarding <casey.r.harding@gmail.com>
2018-04-08 11:34:23 -0400

    Working on views

--- party_house/party/views.py
+++ party_house/party/views.py
@@ -5,15 +5,12 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Party, PartyInformation, Testimonial
 
 def index(request):
-    # all_parties_information = PartyInformation.objects.get()
-    # active_parties = all_parties_information.entry_set.all()
-    # active_parties.filter(active__exact('True'))
+
 
     return render(
         request,
         'index.html',
         context=
         {
-        # 'all_parties_information':all_parties_information,
         }
     )
