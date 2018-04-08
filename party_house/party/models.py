1036af853e5d58f2b35598070cc5f4c7c32c5ca3
CRHarding <casey.r.harding@gmail.com>
2018-04-08 06:30:00 -0400

    Finishing setting up initial database configuration

--- party_house/party/models.py
+++ party_house/party/models.py
@@ -8,17 +8,26 @@ class PartyInformation(models.Model):
     description = models.CharField(max_length=750, help_text="Enter a description of the party (750 max characters!)", blank=False)
     date_of_party = models.DateField('Party Date', blank=False)
 
-    def __str(self):
-        return self.description
+    def get_absolute_url(self):
+        return reverse('party-information-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.address_zip}, {self.description}, {self.date_of_party}'
 
 class Party(models.Model):
     party_information_id = models.ForeignKey('PartyInformation', on_delete=models.SET_NULL, null=True)
-    party_thrower_id = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
-    party_goer_id = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
-    Applications = models.IntegerField(blank=True)
+    party_thrower_id = models.ForeignKey('User', related_name='party_thrower_id', on_delete=models.SET_NULL, null=True)
+    party_goer_id = models.ForeignKey('User', related_name = 'party_goer_id', on_delete=models.SET_NULL, null=True)
+    applications = models.IntegerField(blank=True)
     active = models.BooleanField(help_text="Is the party active? True or False")
     status = models.IntegerField(blank=False, help_text="1 for pending, 2 for accepted, 3 for rejected")
     attended=models.BooleanField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('party-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.applications}, {self.active}'
 
 class User(models.Model):
     f_name = models.CharField(max_length=50, help_text="Enter your first name", blank=False)
@@ -29,8 +38,20 @@ class User(models.Model):
     parties_attended = models.IntegerField(blank=True)
     verified = models.BooleanField()
 
+    def get_absolute_url(self):
+        return reverse('user-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.f_name}, {self.l_name}'
+
 class Testimonial(models.Model):
     party_id = models.ForeignKey('Party', on_delete=models.SET_NULL, null=True)
     user_id = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
     thumbs = models.BooleanField()
     description = models.CharField(max_length=750, help_text="How was this person?", blank=False)
+
+    def get_absolute_url(self):
+        return reverse('testimonial', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.thumbs}, {self.description}'
